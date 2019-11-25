f162250233cc64d4938c4fe859808f61dd8daa0f
Jaiver Andres <jaiverandres98@hotmail.com>
2019-11-25 18:46:54 -0500

    metodo main

--- src/main/java/com/example/demo/FinalOpApplication.java
+++ src/main/java/com/example/demo/FinalOpApplication.java
@@ -19,6 +19,57 @@ public class FinalOpApplication {
 		SpringApplication.run(FinalOpApplication.class, args);
 				
 		
+		try {
+			Runtime runtime = Runtime.getRuntime();
+	        Process proc = runtime.exec("powershell ./src/main/resources/scripts/process.ps1");
+	        InputStream is = proc.getInputStream();
+	        InputStreamReader isr = new InputStreamReader(is);
+	        BufferedReader reader = new BufferedReader(isr);
+	        String line;
+	        String line2;
+	        while ((line = reader.readLine()) != null)
+	        {
+
+	        	System.out.println(line);
+	        	
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test1 = line.split(":");
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test2 = line.split(":");
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test3 = line.split(":");
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test4 = line.split(":");
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test5 = line.split(":");
+//	        		
+//	        		line = reader.readLine();
+//	        		String[] test6 = line.split(":");
+//	        		
+//	        		
+//	        		System.out.println(processArray.size());
+//	        		Proceso p = new Proceso(test1[1], test2[1], test3[1],
+//	        				test4[1], test5[1], test6[1]);
+//	        		processArray.add(p);
+	        	}
+
+
+	        	
+
+	        
+	       
+	        reader.close();
+	        proc.getOutputStream().close();
+		}catch(Exception e) {
+			
+		}
+
+		
 	}
 
 }

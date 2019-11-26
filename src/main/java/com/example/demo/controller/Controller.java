52fec499e908ab5dfd656b407a8154ff6eaa6bfd
Jaiver Andres <jaiverandres98@hotmail.com>
2019-11-25 20:54:47 -0500

    ya esta el agregar, borrar y listar y estan mapeados con el front

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.model.Proceso;
-
 import com.example.demo.repository.ProcesoRepository;
 
 @org.springframework.stereotype.Controller
@@ -49,6 +49,17 @@ public class Controller {
 		model.addAttribute("procesos",procesos);
 		return "listarFormProcesos.html";
 		
+	}
+	
+
+	@RequestMapping(value= "/borrarProceso/{id}", method= RequestMethod.GET)
+	public String handleRequestBorrarUsuarios(Model model, @PathVariable Long id)
+	{
+		Proceso proceso = procesoRepository.findById(id).get();
+		deleteProcess(proceso);
+		List<Proceso> processList = listProcess();
+		model.addAttribute("procesos", processList);
+		return "listarFormUsuarios.html";
 	}
 	
 	

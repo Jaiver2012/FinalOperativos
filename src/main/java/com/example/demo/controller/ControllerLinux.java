2d1a31680e5f4d8a18dc6d0245ccda47b4933415
luisfcv97 <luisfcv97@gmail.com>
2019-11-26 19:35:45 -0500

    finished linux

--- src/main/java/com/example/demo/controller/ControllerLinux.java
+++ src/main/java/com/example/demo/controller/ControllerLinux.java
@@ -2,30 +2,87 @@ package com.example.demo.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import com.example.demo.model.Proceso;
-import com.example.demo.repository.ProcesoRepository;
+
+
 
 @org.springframework.stereotype.Controller
 public class ControllerLinux {
 
-	@Autowired
-	private ProcesoRepository procesoRepository;
-
 	
+	/**
+	 * List different process running in the host with unix
+	 * @return list of process
+	 */
+	public List<Proceso> listProcess(){
+
+		List<Proceso> procesos = new ArrayList<>();
+		try {
+			Process proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "ps -ef" });
+			BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+			String line = reader.readLine();
+
+			while ((line = reader.readLine()) != null)
+			{
+				
+				String[] p = line.split("\\s+");
+				Proceso p1 = new Proceso();
+				
+//				System.out.println(line);
+//				System.out.println(Arrays.toString(p));
+//				System.out.println(p.length);
+				
+				if(p.length==8) {
+					p1.setId(Long.parseLong(p[1]));
+					p1.setNombre(p[7]);
+					p1.setSi(p[0]);
+					p1.setCpu(p[6]);
+					
+				}else {
+					
+					p1.setId(Long.parseLong(p[1]));
+					p1.setSi(p[0]);
+					p1.setCpu(p[6]);
+					p1.setNombre("");
+					for (int i = 7; i < p.length; i++) {
+						p1.setNombre(p1.getNombre()+ " "+p[i]);						
+					}
+				}
+				
+				procesos.add(p1);
+			}
+			
+			
+		}
+		catch (Exception e)
+		{
+			// handle error
+		}
+		return procesos;
+	}
+	
+	
+	
+	/**
+	 * handle of list process
+	 * @param model
+	 * @return page with the process runnig
+	 */
+	@RequestMapping(value = "/listarProcesos/linux", method = RequestMethod.GET)
+	public String listProcessLinux(Model model) {
+
+		List<Proceso> procesos = listProcess();
+		model.addAttribute("procesos", procesos);
+		return "listarFormProcesosLinux.html";
+
+	}
 	
 	/**
 	 * Cancela determinado proceso el cual se le pasa el id por parametro
@@ -36,11 +93,13 @@ public class ControllerLinux {
 	@RequestMapping(value = "/borrarProceso/linux/{id}", method = RequestMethod.GET)
 	public String CancelProcessLinux(Model model, @PathVariable Long id) {
 		
-		Proceso proceso = procesoRepository.findById(id).get();
-		deleteProcess(proceso);
-		//List<Proceso> processList = listProcess();
-		//model.addAttribute("procesos", processList);
-		return "listarFormProcesosWindows.html";
+		try {
+			Process proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "kill "+ id });
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return listProcessLinux(model);
 	}
 	
 	/**
@@ -52,35 +111,5 @@ public class ControllerLinux {
 		return "index.html";
 	}
 
-	@PostConstruct
-	public void listProcess(){
-
-		try {
-			Process proc = Runtime.getRuntime().exec(new String[] { "bash", "-c", "ps aux" });
-			BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-			String line;
-
-			while ((line = reader.readLine()) != null)
-			{
-				System.out.println(line);
-			}
-		}
-		catch (Exception e)
-		{
-			// handle error
-		}
-	}
-	
-	
-	/**
-	 * 
-	 * @param proceso
-	 * @return
-	 */
-	public boolean deleteProcess(Proceso proceso) {
-
-		boolean flag = false;
-		return flag;
-	}
 	
 }
